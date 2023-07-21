0a428e73a2e8261036dce6b67fbb4da64d87898d
bjjumba <ericjjumba5@gmail.com>
2023-07-21 12:51:42 +0300

    Added JsonService

---
+++ Services/JsonFileProductsService.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using CoolApp.Models;
+using Microsoft.AspNetCore.Hosting;
+
+
+namespace CoolApp.Services{
+
+}    

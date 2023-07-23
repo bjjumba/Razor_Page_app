2052ef57dbe2d9b554491ce5b7127fc179167e79
bjjumba <ericjjumba5@gmail.com>
2023-07-23 17:30:57 +0300

    Added a Service

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using CoolApp.Services;
+using CoolApp.Models;
 
 namespace CoolApp.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    public JsonFileProductsService ProductsService;
+    public IEnumerable<Product> Products {get; private set;}
 
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
+       
     }
 
     public void OnGet()
     {
-
+        // Products=ProductsService.GetProducts();
     }
 }
